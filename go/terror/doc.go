dc8cf58e1e1feb41200570fa463f01d365580262
Zane Rockenbaugh <zane@liquid-labs.com>
2019-08-14 14:20:50 -0500

    wdocument debug feature

--- go/terror/doc.go
+++ go/terror/doc.go
@@ -1,7 +1,8 @@
 // The terror package implements typed, user friendly errors in go. The typed errors parallel and are compatible with HTTP errors. A typed error has two basic components:
-//
 // * A user friendly error message meant for viewing by an end-user.
 // * The original underlying error, if any.
-// 
+//
 // Implementations can thus easily support both communicating directly to the user while maintaining all the raw information one would want to capture in a log.
+//
+// terror also supports easy debug logging by either calling 'EchoErrorLog' from your test file or setting the `DEBUG_TERROR` environment variable to any non-blank value.
 package terror
